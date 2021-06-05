01ccce0f93682b788e075c3553e4ff31284c0bda
shazzad504 <82136433+shazzad504@users.noreply.github.com>
2021-06-05 16:02:00 +0600

    Add files via upload

---
+++ lab task/task1.php
@@ -0,0 +1,48 @@
+<html>
+
+<head>
+</head>
+ <body>
+ <form>
+  <h2>
+Registration Form
+<table>
+</h2>
+
+<label for = "frame"> Full Name: </label><br>
+<input type = "chatbox" id = "fname"><br><br>
+
+<label for = "mail"> Email: </label><br>
+<input type = "chatbox" id = "mail"><br><br>
+
+
+<label for = "pass"> : Password:</label><br>
+<input type = "chatbox" id = "pass"><br><br>
+
+<label for = "comment"> Comment:</label><br>
+<input type = "chatbox" id = "comment"><br><br>
+
+<label for = "Gender"> Gender:</label><br>
+
+<input type = "radio"name ="option" id = "Gender">female<br><br>
+<input type = "radio"name ="option" id = "Gender">male<br><br>
+<input type = "radio"name ="option" id = "Gender">other<br>
+
+<label for = "mail"> please chose a hobby: </label><br>
+<input type = "checkbox" id = "Gender">singing<br>
+<input type = "checkbox" id = "Gender">Dancung<br>
+<input type = "checkbox" id = "Gender">reading<br>
+
+
+<form action="/action_page.php">
+  <label for="myfile">Select a file:</label>
+  <input type="file" id="myfile" name="myfile"><br><br>
+  <input type="submit" value="Submit">
+   <input type="Reset" value="Reset">
+  
+
+</table>
+</form>
+</body>
+
+</html>
